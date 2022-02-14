5254e90c1d041176225d3970a90f9d9c7759df57
izacpeterson <izacpeterson@gmail.com>
2022-02-14 09:55:37 -0700

    console logs

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ let data = JSON.parse(fs.readFileSync("./Izacfile").toString());
 console.log(data.msg);
 
 app.get("/", (req, res) => {
+  console.log(`User: ${req.query.name || "anon"} connected`);
   if (req.query.name) res.send("Hello " + req.query.name);
   else res.send("Who are you?");
 });
 
 app.listen(8080, () => {
-  console.log("Server Listening");
+  console.log("Server Listening on 8080");
 });
